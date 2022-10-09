18f8f6806c591b3ea4e5c45737b4a8ce46b2ca1e
MrJackdaw <MrJackdaw@users.noreply.github.com>
2022-10-09 04:48:33 -0700

    * Adds optimism support

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,18 +1,35 @@
+import polygonLogo from "images/bclogo-polygon.png";
+import moonbaseLogo from "images/bclogo-moonbeam.png";
+import klaytnLogo from "images/bclogo-klaytn.png";
+import optimismLogo from "images/bclogo-optimism.png";
+
 // Storage Keys
 export const ANNOUNCER_KEY = "dao-announcer";
 export const DAO_ADDR_KEY = "dao-address";
 export const EVM_CHAIN = "evm-chain";
-export const SupportedEVMs = ["Polygon", "Klaytn", "Moonbase Alpha"];
+const POLYGON = "Polygon";
+const MOONBEAM = "Moonbase Alpha";
+const KLAYTN = "Klaytn";
+const OPTIMISM = "Optimism";
+export const EVMProviders = [
+  { name: "Polygon", value: POLYGON, src: polygonLogo },
+  { name: "Moonbeam", value: MOONBEAM, src: moonbaseLogo },
+  { name: "Klaytn (Baobab)", value: KLAYTN, src: klaytnLogo },
+  { name: "Optimism (Testnet)", value: OPTIMISM, src: optimismLogo }
+];
+export const SupportedEVMs = EVMProviders.map(({ value }) => value);
 
 // Values [mainly contract addresses, though you can change these]
 export const DAO_ANNOUNCER = () => {
   switch (localStorage.getItem(EVM_CHAIN)) {
-    case "Moonbase Alpha":
+    case MOONBEAM:
       return "0x6F8C72BDFf42A1E2561c58da55D901Ba7938e3d4";
-    case "Klaytn":
+    case KLAYTN:
       return "0xd1B0c9e30A82FFEA5A8a052F3eCbf7DbE1108b36";
-    case "Polygon":
+    case POLYGON:
       return "0xD22dF2a68ace2bCE8Cd5770C17a07615A4B4AA09";
+    case OPTIMISM:
+      return "0x6F8C72BDFf42A1E2561c58da55D901Ba7938e3d4";
     default:
       return "";
   }
@@ -24,9 +41,11 @@ export const EVMChainId = (name: string) => {
       return "0x507";
     case "Klaytn":
       return "0x3e9";
-    case "Polygon":
+    case "Polygon": // Mumbai
       return "0x13881";
-    default: // Polygon Mumbai
+    case "Optimism": // Goerli
+      return "0x1a4";
+    default:
       return "0x0";
   }
 };
